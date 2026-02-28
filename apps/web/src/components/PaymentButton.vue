e5a2a62ee7e51e753cb7e2c315ca97818312d910
ohholylymyr-bit <ohholylymyr@gmail.com>
2026-02-28 18:02:10 +0200

    Add PaymentButton component with styling

---
+++ apps/web/src/components/PaymentButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="$emit('pay')" class="payment-button">
+    Pay
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PaymentButton"
+});
+</script>
+
+<style scoped>
+.payment-button {
+  background-color: #0066ff;
+  color: white;
+  border: none;
+  padding: 0.5rem 1rem;
+  cursor: pointer;
+  border-radius: 4px;
+}
+.payment-button:hover { background-color: #0051cc; }
+</style>
